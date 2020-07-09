960630a45cad6f28bbfc69ed5ac8606d7b922898
LikhithaVanga <likhithareddy1994@gmail.com>
2020-07-08 20:44:11 -0700

    Commit katas till 50

--- RomanNumerals/src/main/java/edu/pdx/cs410J/vanga/Kata.java
+++ RomanNumerals/src/main/java/edu/pdx/cs410J/vanga/Kata.java
@@ -10,7 +10,69 @@ public class Kata {
                                                                                     
 
   public static void main(String[] args) {
+
+
     System.err.println("Missing command line arguments");
     System.exit(1);
+  }
+
+  public  static String romanNumeralFor(int i) {
+    String result = "";
+    if(i < 50) {
+      return romanNumeralForLessThan50(i);
+    } else {
+      int numberOfL = i / 50;
+      for (int j = 1; j<= numberOfL;++j) {
+        result += "L";
+      }
+      return result += romanNumeralForLessThan50(i %50);
+    }
+  }
+
+  public  static String romanNumeralForLessThan50(int i) {
+    String result = "";
+    if(i < 10) {
+      return romanNumeralFor1To9(i);
+    } else {
+        int numberOfX = i / 10;
+        for (int j = 1; j<= numberOfX;++j) {
+          result += "X";
+        }
+        return result += romanNumeralFor1To9(i %10);
+    }
+  }
+
+  public  static String romanNumeralFor1To9(int i) {
+    String result = "";
+
+    if( i < 4) {
+      for (int j = 1 ;j <= i; ++j) {
+        result += "I";
+      }
+    }
+
+    if (i ==4) {
+      result += "IV";
+    }
+
+    if (i ==5) {
+      return "V";
+    }
+    if (i ==6) {
+      return "VI";
+    }
+    if (i ==7) {
+      return "VII";
+    }
+    if (i ==8) {
+      return "VIII";
+    }
+    if (i == 9) {
+      return "IX";
+    }
+
+    return result;
+
+
   }
 }
--- RomanNumerals/src/test/java/edu/pdx/cs410J/vanga/KataTest.java
+++ RomanNumerals/src/test/java/edu/pdx/cs410J/vanga/KataTest.java
@@ -1,13 +1,86 @@
 package edu.pdx.cs410J.vanga;
 
 import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.*;
 
 public class KataTest
 {
 
   @Test
-  public void canInstantiateKataClass() {
-    new Kata();
+  public void given1ReturnI() {
+    assertThat(Kata.romanNumeralFor(1), equalTo("I"));
+  }
+
+  @Test
+  public void given2ReturnII() {
+    assertThat(Kata.romanNumeralFor(2), equalTo("II"));
+  }
+
+  @Test
+  public void given3ReturnIII() {
+    assertThat(Kata.romanNumeralFor(3), equalTo("III"));
+  }
+
+  @Test
+  public void given4ReturnIV() {
+    assertThat(Kata.romanNumeralFor(4), equalTo("IV"));
+  }
+
+  @Test
+  public void given5ReturnV() {
+    assertThat(Kata.romanNumeralFor(5), equalTo("V"));
+  }
+
+  @Test
+  public void given9ReturnIX() {
+    assertThat(Kata.romanNumeralFor(9), equalTo("IX"));
+  }
+
+
+  @Test
+  public void given10ReturnX() {
+    assertThat(Kata.romanNumeralFor(10), equalTo("X"));
+  }
+  @Test
+  public void given11ReturnXI() {
+    assertThat(Kata.romanNumeralFor(11), equalTo("XI"));
+  }
+
+  @Test
+  public void given20ReturnXX() {
+    assertThat(Kata.romanNumeralFor(20), equalTo("XX"));
+  }
+  @Test
+  public void given29ReturnXXIX() {
+    assertThat(Kata.romanNumeralFor(29), equalTo("XXIX"));
+  }
+
+  @Test
+  public void given32ReturnXXXII() {
+    assertThat(Kata.romanNumeralFor(32), equalTo("XXXII"));
+  }
+
+  @Test
+  public void given35ReturnXXXV() {
+    assertThat(Kata.romanNumeralFor(35), equalTo("XXXV"));
+  }
+  @Test
+  public void given36ReturnXXXVI() {
+    assertThat(Kata.romanNumeralFor(36), equalTo("XXXVI"));
+  }
+  @Test
+  public void given37ReturnXXXVII() {
+    assertThat(Kata.romanNumeralFor(37), equalTo("XXXVII"));
+  }
+  @Test
+  public void given38ReturnXXXVIII() {
+    assertThat(Kata.romanNumeralFor(38), equalTo("XXXVIII"));
+  }
+
+  @Test
+  public void given49ReturnIL() {
+    assertThat(Kata.romanNumeralFor(50), equalTo("L"));
   }
 
 }
